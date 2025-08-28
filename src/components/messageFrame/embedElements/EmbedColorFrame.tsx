d878521e9f8a90d3aee9d18269d75723dab7057a
RedFrOzzi <redfrozzi@gmail.com>
2025-08-28 12:08:14 +0300

    Changed color picker

--- src/components/messageFrame/embedElements/EmbedColorFrame.tsx
+++ src/components/messageFrame/embedElements/EmbedColorFrame.tsx
@@ -1,12 +1,40 @@
-import { useState } from "react";
-import { HexColorPicker } from "react-colorful";
+import { RgbColor, RgbColorPicker } from "react-colorful";
+import "../../../styles/embedElements/EmbedColorFrame.css";
+import { useEffect, useState } from "react";
 
-export default function EmbedColorFrame() {
-  const [color, setColor] = useState<string>("#ffffff");
+interface EmbedColorFrameArgs {
+  setColorCallback: (c: RgbColor) => void;
+  closePanelCallback: () => void;
+}
+
+export default function EmbedColorFrame(args: EmbedColorFrameArgs) {
+  const [color, setColor] = useState<RgbColor>({ r: 0, g: 0, b: 0 });
+  const [textColor, setTextColor] = useState<string>("white");
+
+  useEffect(() => {
+    args.setColorCallback(color);
+    const brightness = (color.r * 299 + color.g * 587 + color.b * 114) / 1000;
+    brightness > 150 ? setTextColor("black") : setTextColor("white");
+  }, [color]);
 
   return (
-    <div>
-      <HexColorPicker color={color} onChange={setColor} />
+    <div id="embed_color_picker" onClick={(e) => e.stopPropagation()}>
+      <div
+        id="embed_color_picker_backpanel"
+        onClick={() => args.closePanelCallback()}
+      ></div>
+      <div id="hexcolorpicker">
+        <RgbColorPicker onChange={setColor} />
+        <button
+          style={{
+            backgroundColor: `rgb(${color.r}, ${color.g}, ${color.b})`,
+            color: textColor,
+          }}
+          onClick={() => args.closePanelCallback()}
+        >
+          OK
+        </button>
+      </div>
     </div>
   );
 }
--- src/components/messageFrame/embedElements/EmbedFrame.tsx
+++ src/components/messageFrame/embedElements/EmbedFrame.tsx
@@ -7,6 +7,7 @@ import EmbedBuilder from "../../../utils/EmbedBuilder";
 import EmbedTextFrame from "./EmbedTextFrame";
 import EmbedFieldFrame from "./EmbedFieldFrame";
 import EmbedColorFrame from "./EmbedColorFrame";
+import { RgbColor } from "react-colorful";
 
 interface EmbedFrameArgs {
   args: IMessageFrameArgs;
@@ -16,7 +17,7 @@ interface EmbedFrameArgs {
 export default function EmbedFrame(eArgs: EmbedFrameArgs) {
   const [isOptionsOpen, setOptionsState] = useState(false);
   const [isColorPickerOpen, setColorPickerState] = useState(false);
-  const [sideColor, setSideColor] = useState<string>("#ffffff");
+  const [sideColor, setSideColor] = useState<RgbColor>({ r: 0, g: 0, b: 0 });
 
   function onSelectOption(item: { index: number; name: string } | null) {
     if (item === null) {
@@ -36,36 +37,50 @@ export default function EmbedFrame(eArgs: EmbedFrameArgs) {
           }}
         />
       ) : null}
-      <div id="embed_frame">
-        <div
-          id="embed_color"
-          style={{ backgroundColor: sideColor }}
-          onClick={() => setColorPickerState(true)}
-        >
-          {isColorPickerOpen ? <EmbedColorFrame /> : null}
-        </div>
-        <div id="embed_contructor_frame">
-          <div id="constructor">
-            {eArgs.builder.hasTitle ? (
-              <div className="embed_element">
-                <EmbedTextFrame placeholder="Заглавие..." fontSize="2.4ch" />
-              </div>
+      <div id="embed_frame_scroll_container">
+        <div id="embed_frame">
+          <div
+            id="embed_color"
+            style={{
+              backgroundColor: `rgb(${sideColor.r}, ${sideColor.g}, ${sideColor.b})`,
+            }}
+            onClick={() => setColorPickerState(true)}
+          >
+            {isColorPickerOpen ? (
+              <EmbedColorFrame
+                setColorCallback={setSideColor}
+                closePanelCallback={() => {
+                  setColorPickerState(false);
+                }}
+              />
             ) : null}
-            {eArgs.builder.hasDescription ? (
-              <div className="embed_element">
-                <EmbedTextFrame placeholder="Описание..." fontSize="2ch" />
-              </div>
-            ) : null}
-            {eArgs.builder.hasUrl ? (
-              <div className="embed_element">
-                <a href={eArgs.builder.embed.url!}>eArgs.builder.embed.url</a>
-              </div>
-            ) : null}
-            {EnumerateFields(eArgs.builder.fieldsCount)}
           </div>
-          <button id="add_element_button" onClick={() => setOptionsState(true)}>
-            <PlusIcon heigth="20px" width="20px" />
-          </button>
+          <div id="embed_contructor_frame">
+            <div id="constructor">
+              {eArgs.builder.hasTitle ? (
+                <div className="embed_element">
+                  <EmbedTextFrame placeholder="Заглавие..." fontSize="2.4ch" />
+                </div>
+              ) : null}
+              {eArgs.builder.hasDescription ? (
+                <div className="embed_element">
+                  <EmbedTextFrame placeholder="Описание..." fontSize="2ch" />
+                </div>
+              ) : null}
+              {eArgs.builder.hasUrl ? (
+                <div className="embed_element">
+                  <a href={eArgs.builder.embed.url!}>eArgs.builder.embed.url</a>
+                </div>
+              ) : null}
+              {EnumerateFields(eArgs.builder.fieldsCount)}
+            </div>
+            <button
+              id="add_element_button"
+              onClick={() => setOptionsState(true)}
+            >
+              <PlusIcon heigth="20px" width="20px" />
+            </button>
+          </div>
         </div>
       </div>
     </>
