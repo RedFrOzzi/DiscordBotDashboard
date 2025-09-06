77ce29ab6e1ba8bec8dfb58aba2149513c051a58
RedFrOzzi <128400196+RedFrOzzi@users.noreply.github.com>
2025-09-06 21:45:40 +0300

    Added search

--- src/components/usersList/UsersListMainContainer.tsx
+++ src/components/usersList/UsersListMainContainer.tsx
@@ -1,6 +1,8 @@
+import { useEffect, useRef, useState } from "react";
 import User from "../../models/User";
 import "../../styles/usersListMainContainer/UsersListMainContainer.css";
 import UserContainer from "./UserContainer";
+import fuzzysort from "fuzzysort";
 
 interface UsersListMainContainerArgs {
   users: User[];
@@ -9,13 +11,43 @@ interface UsersListMainContainerArgs {
 export default function UsersListMainContainer(
   args: UsersListMainContainerArgs
 ) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [inputText, setInputText] = useState<string>("");
+  const [filteredUsers, setFilteredUsers] = useState<User[]>(args.users);
+
+  useEffect(() => {
+    if (inputText === null || inputText === "") {
+      setFilteredUsers(args.users);
+      return;
+    }
+
+    const results = fuzzysort.go(inputText, args.users, {
+      threshold: 0.5,
+      all: false,
+      keys: ["username", "nickname", "globalName"],
+    });
+
+    if (results.length === 0) {
+      setFilteredUsers([]);
+      return;
+    }
+
+    setFilteredUsers(results.map((item) => item.obj));
+  }, [inputText]);
+
   return (
     <>
       <div id="users_list_search">
-        <input type="text" placeholder="Поиск" />
+        <input
+          ref={inputRef}
+          type="text"
+          placeholder="Поиск"
+          value={inputText}
+          onChange={(e) => setInputText(e.target.value)}
+        />
       </div>
       <div id="users_list_container">
-        {args.users.map((user, index) => (
+        {filteredUsers.map((user, index) => (
           <UserContainer user={user} key={index} />
         ))}
       </div>
